2cfd06300f528a3f1c6ce2976d9cb108e4d699c1
NatashaPletniova <pletniovan@gmail.com>
2017-02-12 15:27:55 +0300

    Task_2 was added

---
+++ Task_2/Lesson22.java
@@ -0,0 +1,14 @@
+package by.epam.lesson2.homework;
+
+public class Lesson22 {
+	public static void main(String[] args) {
+		//Выведите на консоль ваши ФИО, адрес и телефон.
+	
+		System.out.println("This is the second task!");
+		System.out.println();
+		System.out.println("Krishtapovich Natallia Iosifovna");
+		System.out.println("Minsk Parnikovaya st. 9-31 ");
+		System.out.println("+375296317917");
+	}
+
+}
